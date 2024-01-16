7a7e1f13a29b29fa07ff6a928e64ed680d5a9144
Taner <tanerileri25@gmail.com>
2024-01-16 21:10:38 +0300

    update

--- InternshipManagementSystem.API/Core/InternshipManagementSystem.Domain/Entities/Identity/AppRole.cs
+++ InternshipManagementSystem.API/Core/InternshipManagementSystem.Domain/Entities/Identity/AppRole.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace InternshipManagementSystem.Domain.Entities.Identity
 {
-    internal class AppRole
+    public class AppRole : IdentityRole<string>
     {
     }
 }
