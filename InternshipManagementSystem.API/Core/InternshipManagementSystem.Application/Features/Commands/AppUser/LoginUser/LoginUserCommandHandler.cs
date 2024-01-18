ee0a8da8115cccbde99a8d8675ba6d8b6b6097ce
Taner <tanerileri25@gmail.com>
2024-01-19 01:53:06 +0300

    update

---
+++ InternshipManagementSystem.API/Core/InternshipManagementSystem.Application/Features/Commands/AppUser/LoginUser/LoginUserCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipManagementSystem.Application.Features.Commands.AppUser.LoginUser
+{
+    public class LoginUserCommandHandler : IRequestHandler<LoginUserCommandRequest, LoginUserCommandResponse>
+    {
+        public async Task<LoginUserCommandResponse> Handle(LoginUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            return null;
+        }
+    }
+}
--- InternshipManagementSystem.API/Core/InternshipManagementSystem.Domain/Entities/Identity/AppUser.cs
+++ InternshipManagementSystem.API/Core/InternshipManagementSystem.Domain/Entities/Identity/AppUser.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InternshipManagementSystem.Domain.Entities.AppUser
+namespace InternshipManagementSystem.Domain.Entities.Identity
 {
     public class AppUser : IdentityUser<string>
     {
