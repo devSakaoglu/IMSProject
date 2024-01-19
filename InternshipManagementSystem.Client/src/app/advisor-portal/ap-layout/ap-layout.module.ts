d362acca01818069582d66d334bc0e88e0662c88
Koray Kocaelli <koraykocaelli@outlook.com>
2024-01-19 04:00:28 +0300

    update

--- InternshipManagementSystem.Client/src/app/advisor-portal/ap-layout/ap-layout.module.ts
+++ InternshipManagementSystem.Client/src/app/advisor-portal/ap-layout/ap-layout.module.ts
@@ -4,18 +4,12 @@ import { ApLayoutComponent } from './ap-layout.component';
 import { RouterModule } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { ComponentsModule } from '../components/components.module';
-import { ApHeaderComponent } from './components/ap-header/ap-header.component';
-import { ApSidebarComponent } from './components/ap-sidebar/ap-sidebar.component';
-import { ApFooterComponent } from './components/ap-footer/ap-footer.component';
 
 
 
 @NgModule({
   declarations: [
     ApLayoutComponent,
-    ApHeaderComponent,
-    ApSidebarComponent,
-    ApFooterComponent
   ],
   imports: [
     CommonModule,
