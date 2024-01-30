679620f72d103c0d9abe97d5bff385710744a24d
Talha Sakaoglu <mtsakaoglu@gmail.com>
2024-01-30 21:35:33 +0300

    student controller modified

--- InternshipManagementSystem.API/Presentation/InternshipManagementSystem.API/Controllers/StudentsController.cs
+++ InternshipManagementSystem.API/Presentation/InternshipManagementSystem.API/Controllers/StudentsController.cs
@@ -9,115 +9,117 @@ using Microsoft.EntityFrameworkCore;
 
 namespace InternshipManagementSystem.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class StudentsController : ControllerBase
-    {
-        private readonly IStudentReadRepository _studentReadRepository;
-        private readonly IStudentWriteRepository _studentWriteRepository;
-        private readonly IAdvisorReadRepository _advisorReadRepository;
-        private readonly IAdvisorWriteRepository _advisorWriteRepository;
-        private readonly IWebHostEnvironment _webHostEnvironment;
-        private readonly IFileService _fileService;
+   [Route("api/[controller]")]
+   [ApiController]
+   public class StudentsController : ControllerBase
+   {
+      private readonly IStudentReadRepository _studentReadRepository;
+      private readonly IStudentWriteRepository _studentWriteRepository;
+      private readonly IAdvisorReadRepository _advisorReadRepository;
+      private readonly IAdvisorWriteRepository _advisorWriteRepository;
+      private readonly IWebHostEnvironment _webHostEnvironment;
+      private readonly IFileService _fileService;
 
-        public StudentsController(IStudentReadRepository studentReadRepository, IStudentWriteRepository studentWriteRepository, IAdvisorReadRepository advisorReadRepository, IAdvisorWriteRepository advisorWriteRepository, IWebHostEnvironment webHostEnvironment, IFileService fileService)
-        {
-            _studentReadRepository = studentReadRepository;
-            _studentWriteRepository = studentWriteRepository;
-            _advisorReadRepository = advisorReadRepository;
-            _advisorWriteRepository = advisorWriteRepository;
-            _webHostEnvironment = webHostEnvironment;
-            _fileService = fileService;
-        }
+      public StudentsController(IStudentReadRepository studentReadRepository, IStudentWriteRepository studentWriteRepository, IAdvisorReadRepository advisorReadRepository, IAdvisorWriteRepository advisorWriteRepository, IWebHostEnvironment webHostEnvironment, IFileService fileService)
+      {
+         _studentReadRepository = studentReadRepository;
+         _studentWriteRepository = studentWriteRepository;
+         _advisorReadRepository = advisorReadRepository;
+         _advisorWriteRepository = advisorWriteRepository;
+         _webHostEnvironment = webHostEnvironment;
+         _fileService = fileService;
+      }
 
-        [HttpGet]
-        public async Task<IActionResult> Get()
+      [HttpGet]
+      public async Task<IActionResult> Get()
 
-        {
-            var x = _studentReadRepository.GetAll();
-            return Ok(x);
-        }
-        [HttpGet("{id}")]
-        public async Task<IActionResult> Get(string id)
-        {
-            var advisor = await _studentReadRepository.GetByIdAsync(id, false);
-            return Ok(advisor);
-        }
-        [HttpPost("[action]")]
-        public async Task<IActionResult> Add(VM_Create_Student model)
-        {
-            Student student = await _studentReadRepository.GetSingleAsync(x => x.TC_NO == model.TC_NO || x.StudentNo == model.StudentNo, false);
-            if (student is null)
-            {
-                return Ok(new ResponseModel()
-                {
-                    IsSuccess = false,
-                    Message = $"There is not a student with these properties",
-                    Data = null,
-                    StatusCode = 400
-
-                });
-
-            }
-
-
-
-            var advisor = await _advisorReadRepository.GetAll().Include(x => x.Students).FirstOrDefaultAsync(x => x.ID == model.AdvisorID);
-            if (advisor != null)
-            {
-                try
-                {
-                    advisor.Students.Add(student);
-                    await _advisorWriteRepository.SaveAsync();
-                    await _studentWriteRepository.SaveAsync();
-                    return Ok(new ResponseModel()
-                    {
-                        IsSuccess = true,
-                        Message = "Successful",
-                        Data = student,
-                        StatusCode = 200
-
-                    });
-                }
-                catch (Exception ex)
-                {
-                    return Ok(new ResponseModel()
-                    {
-                        IsSuccess = false,
-                        Message = ex.Message,
-                        Data = null,
-                        StatusCode = 400
-
-                    });
-                }
-
-            }
+      {
+         var x = _studentReadRepository.GetAll();
+         return Ok(x);
+      }
+      [HttpGet("{id}")]
+      public async Task<IActionResult> Get(string id)
+      {
+         var advisor = await _studentReadRepository.GetByIdAsync(id, false);
+         return Ok(advisor);
+      }
+      [HttpPost("[action]")]
+      public async Task<IActionResult> AddToAdvisor(VM_Add_Student_to_Advisor model)
+      {  
+         Student student = await _studentReadRepository.GetSingleAsync(s => s.ID == model.StudentID, true);
+         if (student is null)
+         {
             return Ok(new ResponseModel()
             {
-                IsSuccess = false,
-                Message = "Some problems",
-                Data = null,
-                StatusCode = 499
+               IsSuccess = false,
+               Message = $"There is not a student with these properties",
+               Data = null,
+               StatusCode = 400
 
             });
-        }
-        [HttpPost]
-        public async Task<IActionResult> Post(VM_Create_Student model)
-        {
-            Student e = _studentReadRepository.GetFirst(x => x.TC_NO == model.TC_NO || x.StudentNo == model.StudentNo, false);
-            if (e is not null)
-            {
-                var str = e.StudentNo == model.StudentNo ? "Student Number" : "TC Number";
-                return Ok(new ResponseModel()
-                {
-                    IsSuccess = false,
-                    Message = $"There is a student with same {str} number",
-                    Data = null,
-                    StatusCode = 400
 
-                });
+         }
+
+
+
+         var advisor = await _advisorReadRepository.GetAll().Include(x => x.Students).FirstOrDefaultAsync(x => x.ID == model.AdvisorID);
+         if (advisor != null)
+         {
+            try
+            {
+               advisor.Students.Add(student);
+               var x = await _advisorWriteRepository.SaveAsync();
+               var y = await _studentWriteRepository.SaveAsync();
+               var res = new ResponseModel()
+               {
+                  IsSuccess = true, 
+                  Message = "Successful",
+                  Data = student,
+                  StatusCode = 200
+
+               };
+               return Ok(res);
             }
-            await _studentWriteRepository.AddAsync(new Student()
+            catch (Exception ex)
+            {
+               return Ok(new ResponseModel()
+               {
+                  IsSuccess = false,
+                  Message = ex.Message,
+                  Data = null,
+                  StatusCode = 400
+
+               });
+            }
+
+         }
+         return Ok(new ResponseModel()
+         {
+            IsSuccess = false,
+            Message = "Some problems",
+            Data = null,
+            StatusCode = 499
+
+         });
+
+      }
+      [HttpPost]
+      public async Task<IActionResult> Post(VM_Create_Student model)
+      {
+         Student e = _studentReadRepository.GetFirst(x => x.TC_NO == model.TC_NO || x.StudentNo == model.StudentNo, false);
+         if (e is not null)
+         {
+            var str = e.StudentNo == model.StudentNo ? "Student Number" : "TC Number";
+            return Ok(new ResponseModel()
+            {
+               IsSuccess = false,
+               Message = $"There is a student with same {str} number",
+               Data = null,
+               StatusCode = 400
+
+            });
+         }
+         Student student =new()
             {
                 Address = model.Address,
                 Email = model.Email,
@@ -130,85 +132,89 @@ namespace InternshipManagementSystem.API.Controllers
                 DepartmentName = model.DepartmentName,
                 ProgramName = model.ProgramName,
                 FacultyName = model.FacultyName,
-            });
-            if (await _studentWriteRepository.SaveAsync() == 1)
-            {
-                return Ok(
-                   new ResponseModel(true, "Student added", null, 200)
+            };
 
-                   );
-
-            }
-            else
-            {
-                return Ok(
-                       new ResponseModel(false, "Some problems", null, 400)
-                                           );
-            }
+       bool progres =   await _studentWriteRepository.AddAsync(student);
 
 
-
-
-        }
-
-        [HttpPut]
-        public async Task<IActionResult> Update(VM_Update_Student model)
-        {
-            var student = await _studentReadRepository.GetByIdAsync(model.StudentID.ToString());
-
-            if (student is null)
-            {
-                return Ok(new ResponseModel()
-                {
-                    IsSuccess = false,
-                    Message = "Student not found",
-                    Data = null,
-                    StatusCode = 400
-
-                });
-            }
-
-            student.TC_NO = model.TC_NO;
-            student.Email = model.Email;
-            student.Address = model.Address;
-            student.StudentName = model.StudentName;
-            student.StudentSurname = model.StudentSurname;
-            student.StudentNo = model.StudentNo;
-            student.StudentGSMNumber = model.StudentGSMNumber;
-            student.GPA = model.GPA;
-            student.DepartmentName = model.DepartmentName;
-            student.ProgramName = model.ProgramName;
-            await _studentWriteRepository.SaveAsync();
+         if (await _studentWriteRepository.SaveAsync() == 1 )
+         {
             return Ok(
-               new ResponseModel(true, "Succesful", student, 200)
+               new ResponseModel(true, "Student added", student, 200)
+
                );
-        }
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(string id)
-        {
-            await _studentWriteRepository.RemoveAsync(id);
-            await _studentWriteRepository.SaveAsync();
-            return Ok(
-                new
-                {
-                    Deletion_Process = "Successful",
-                    StatusCode = 200
-                });
-        }
-        [HttpPost("[action]")]
 
-        public async Task<IActionResult> Upload([FromForm] IFormFileCollection file,string StudentID = "2",string InternshipID ="1")
-        {
-          var data = await  _fileService.UploadAsync($"Students\\{StudentID}\\{InternshipID}\\", file);
+         }
+         else
+         {
+            return Ok(
+                   new ResponseModel(false, "Some problems", null, 400)
+                                       );
+         }
+
+
+
+
+      }
+
+      [HttpPut]
+      public async Task<IActionResult> Update(VM_Update_Student model)
+      {
+         var student = await _studentReadRepository.GetByIdAsync(model.StudentID.ToString());
+
+         if (student is null)
+         {
             return Ok(new ResponseModel()
             {
-                Data=data.ToDictionary(),
-                IsSuccess=true,
-                Message="Successful",
-                StatusCode=200
-            });
-          
+               IsSuccess = false,
+               Message = "Student not found",
+               Data = null,
+               StatusCode = 400
 
-        }
-    }
+            });
+         }
+
+         student.TC_NO = model.TC_NO;
+         student.Email = model.Email;
+         student.Address = model.Address;
+         student.StudentName = model.StudentName;
+         student.StudentSurname = model.StudentSurname;
+         student.StudentNo = model.StudentNo;
+         student.StudentGSMNumber = model.StudentGSMNumber;
+         student.GPA = model.GPA;
+         student.DepartmentName = model.DepartmentName;
+         student.ProgramName = model.ProgramName;
+         await _studentWriteRepository.SaveAsync();
+         return Ok(
+            new ResponseModel(true, "Succesful", student, 200)
+            );
+      }
+      [HttpDelete("{id}")]
+      public async Task<IActionResult> Delete(string id)
+      {
+         await _studentWriteRepository.RemoveAsync(id);
+         await _studentWriteRepository.SaveAsync();
+         return Ok(
+             new
+             {
+                Deletion_Process = "Successful",
+                StatusCode = 200
+             });
+      }
+      [HttpPost("[action]")]
+
+      public async Task<IActionResult> Upload([FromForm] IFormFileCollection file, string StudentID = "2", string InternshipID = "1")
+      {
+         var data = await _fileService.UploadAsync($"Students\\{StudentID}\\{InternshipID}\\", file);
+         return Ok(new ResponseModel()
+         {
+            Data = data.ToDictionary(),
+            IsSuccess = true,
+            Message = "Successful",
+            StatusCode = 200
+         });
+
+
+      }
+   }
 }
