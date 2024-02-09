197595d4b2a88f0c90dac205960cd30149d0dd0f
Talha Sakaoglu <mtsakaoglu@gmail.com>
2024-02-09 10:33:23 +0300

    statuses changed

--- InternshipManagementSystem.API/Core/InternshipManagementSystem.Domain/Entities/InternshipStatus.cs
+++ InternshipManagementSystem.API/Core/InternshipManagementSystem.Domain/Entities/InternshipStatus.cs
@@ -2,12 +2,19 @@
 {
     public enum InternshipStatus
     {
-        ApplicationPending = 0,
-        ApplicationApproved = 1,
-        InternshipStarted = 2,
-        InternshipCompleted = 3,
-        Canceled = 4,
-        UnderReview = 5,
-        MustBeUpdate = 6
+        Deleted = -1, // Silindi
+        Pending = 0, // Beklemede
+        ApplicationRejected = 1, // Staj başvurusu reddedildi
+        ModificationRequiredInStartInformation = 2, // Staj başlatma bilgilerinde düzenleme gerekli
+        FilesAwaited = 3, // Staj dosyaları bekleniyor
+        FilesUnderReview = 4, // Staj dosyaları incelemede
+        FilesApproved = 5, // Staj dosyaları onaylandı
+        DocumentsAwaitingSubmission = 6, // Staj evrakları teslim bekleniyor
+        DocumentsSubmitted = 7, // Staj evrakları teslim edildi
+        DocumentsNotSubmitted = 8, // Staj evrakları teslim edilmedi
+        DocumentsUnderReview = 9, // Staj evrakları kontrol ediliyor
+        InternshipCompleted = 10, // Staj tamamlandı
+        InternshipCanceled = 11 // Staj iptal edildi
     }
+
 }
