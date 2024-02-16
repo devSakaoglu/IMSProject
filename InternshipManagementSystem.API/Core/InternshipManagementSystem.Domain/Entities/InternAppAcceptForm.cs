1cf54dcc57471a5b6711917f8361176e6611575c
Talha Sakaoglu <mtsakaoglu@gmail.com>
2024-02-16 16:00:08 +0300

    some updates about general

--- InternshipManagementSystem.API/Core/InternshipManagementSystem.Domain/Entities/InternAppAcceptForm.cs
+++ InternshipManagementSystem.API/Core/InternshipManagementSystem.Domain/Entities/InternAppAcceptForm.cs
@@ -1,29 +1,10 @@
-﻿namespace InternshipManagementSystem.Domain.Entities
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace InternshipManagementSystem.Domain.Entities
 {
 
-   public class InternAppAcceptForm : BaseEntity
+   public class InternshipApplicationForm : InternshipDocument
    {
-        //Todo document olacak entity olmuyacak
-      public Guid? InternshipID { get; set; }
-
-      public string StudentName { get; set; }
-      public string StudentSurname { get; set; }
-      public string TC_NO { get; set; }
-      public bool SPAS { get; set; }
-      public string CompanyName { get; set; }
-      public string CompanyAddress { get; set; }
-      public string FieldOfActivity { get; set; }
-      public string GSMNumber { get; set; }
-      public string ManagerEmail { get; set; }
-      public string ManagerFullName { get; set; }
-      public string ManagerPosition { get; set; }
-      public string StartDatePlan { get; set; }
-      public string EndDatePlan { get; set; }
-      public bool IsPaid { get; set; }
-      public double Price { get; set; }
-      public string Days { get; set; }
-
-
    }
 
 }
