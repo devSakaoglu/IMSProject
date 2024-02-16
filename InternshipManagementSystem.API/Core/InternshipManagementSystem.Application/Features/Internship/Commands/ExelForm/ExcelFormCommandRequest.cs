950421b3013cbabc3902daa4fc19298d07096fd8
Taner <Taner@DESKTOP-JD058BL>
2024-02-16 21:38:01 +0300

    update

---
+++ InternshipManagementSystem.API/Core/InternshipManagementSystem.Application/Features/Internship/Commands/ExelForm/ExcelFormCommandRequest.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipManagementSystem.Application.Features.Internship.Commands.ExelForm
+{
+    public class ExcelFormCommandRequest : IRequest<ExcelFormCommandResponse>
+    {
+
+
+    }
+}
