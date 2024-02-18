b843fa08e236e4700498508fd9894acd4c7fc4b6
Taner <Taner@DESKTOP-JD058BL>
2024-02-18 22:42:23 +0300

    update

---
+++ InternshipManagementSystem.API/Core/InternshipManagementSystem.Application/Features/Advisor/Queries/GetAdvisorById/GetAdvisorByIdQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using InternshipManagementSystem.Application.Repositories;
+using InternshipManagementSystem.Application.ViewModels;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipManagementSystem.Application.Features.Advisor.Queries.GetAdvisorById
+{
+    public class GetAdvisorByIdQueryHandler : IRequestHandler<GetAdvisorByIdQueryRequest, GetAdvisorByIdQueryResponse>
+    {
+        readonly IAdvisorReadRepository _advisorReadRepository;
+
+        public GetAdvisorByIdQueryHandler(IAdvisorReadRepository advisorReadRepository)
+        {
+            _advisorReadRepository = advisorReadRepository;
+        }
+
+        public async Task<GetAdvisorByIdQueryResponse> Handle(GetAdvisorByIdQueryRequest request, CancellationToken cancellationToken)
+        {
+            var advisor = await _advisorReadRepository.GetByIdAsync(request.Id, false);
+            return new GetAdvisorByIdQueryResponse
+            { 
+                Response= new ResponseModel
+                {
+                    IsSuccess = true,
+                    Message= "Successful",
+                    Data= advisor,
+                    StatusCode=200
+                }
+
+            };
+              
+        }
+    }
+}
--- InternshipManagementSystem.API/Presentation/InternshipManagementSystem.API/Controllers/AdvisorController.cs
+++ InternshipManagementSystem.API/Presentation/InternshipManagementSystem.API/Controllers/AdvisorController.cs
@@ -1,8 +1,15 @@
 ﻿using InternshipManagementSystem.API.Constants;
+using InternshipManagementSystem.Application.Features.Advisor.Commands.CreateAdvisor;
+using InternshipManagementSystem.Application.Features.Advisor.Commands.DeleteAdvisorById;
+using InternshipManagementSystem.Application.Features.Advisor.Commands.UpdateAdvisor;
+using InternshipManagementSystem.Application.Features.Advisor.Queries.GetAdvisorById;
+using InternshipManagementSystem.Application.Features.Advisor.Queries.GetAllAdvisor;
+using InternshipManagementSystem.Application.Features.Advisor.Queries.GetStudentsOfAllAdvisors;
 using InternshipManagementSystem.Application.Repositories;
 using InternshipManagementSystem.Application.ViewModels;
 using InternshipManagementSystem.Application.ViewModels.AdvisorViewModels;
 using InternshipManagementSystem.Domain.Entities;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -18,186 +25,62 @@ namespace InternshipManagementSystem.API.Controllers
         readonly private IAdvisorWriteRepository _advisorWriteRepository;
         readonly private IStudentReadRepository _studentReadRepository;
         readonly private IStudentWriteRepository _studentWriteRepository;
+        readonly private IMediator _mediator;
 
-        public AdvisorController(IAdvisorReadRepository advisorReadRepository, IAdvisorWriteRepository advisorWriteRepository, IStudentReadRepository studentReadRepository, IStudentWriteRepository studentWriteRepository)
+        public AdvisorController(IAdvisorReadRepository advisorReadRepository, IAdvisorWriteRepository advisorWriteRepository, IStudentReadRepository studentReadRepository, IStudentWriteRepository studentWriteRepository, IMediator mediator)
         {
             _advisorReadRepository = advisorReadRepository;
             _advisorWriteRepository = advisorWriteRepository;
             _studentReadRepository = studentReadRepository;
             _studentWriteRepository = studentWriteRepository;
+            _mediator = mediator;
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> Get(Guid id)
+        public async Task<IActionResult> GetAdvisorById(GetAdvisorByIdQueryRequest request)
         {
-            var advisor = await _advisorReadRepository.GetByIdAsync(id, false);
-            return Ok(
-               new ResponseModel(true, "Successful", advisor, 200)
-               );
-
+           GetAdvisorByIdQueryResponse response= await _mediator.Send(request);
+            return Ok(response);
         }
 
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> GetAll(GetAllAdvisorQueryRequest request)
         {
-            var data = _advisorReadRepository.GetAll(false);
-            return Ok(
-            new ResponseModel(true, "Successful", data, 200)
-         );
+            GetAllAdvisorQueryResponse response= await _mediator.Send(request);
+            return Ok(response);
         }
 
         [HttpGet("[action]")]
-        public async Task<IActionResult> GetStudentsOfAllAdvisors()
+        public async Task<IActionResult> GetStudentsOfAllAdvisors(GetStudentsOfAllAdvisorsQueryRequest request)
         {
-            var data = _advisorReadRepository.GetAll().Include(x => x.Students);
-            var students = data.SelectMany(x => x.Students).ToList();
-            return Ok(
-                           new ResponseModel(true, "Successful", students, 200)
-                                   );
-
-
+           GetStudentsOfAllAdvisorsQueryResponse response= await _mediator.Send(request);
+            return Ok(response);
         }
         [HttpGet("[action]/{id}")]
-        public async Task<IActionResult> GetStudentsOfAdvisor(string id)
+        public async Task<IActionResult> GetStudentsOfAdvisor(GetStudentsOfAllAdvisorsQueryRequest request)
         {
-            var data = await _advisorReadRepository.Table.Include(x => x.Students).FirstOrDefaultAsync(x => x.ID == Guid.Parse(id));
-            var students = data.Students;
-            return Ok(
-                                          new ResponseModel(true, "Successful", students, 200)
-                                                                            );
+            GetStudentsOfAllAdvisorsQueryResponse response = await _mediator.Send(request);
+            return Ok(response);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post(VM_Create_Advisor model)
+        public async Task<IActionResult> Create(CreateAdvisorCommandRequest request)
         {
-            Advisor e = await _advisorReadRepository.GetSingleAsync(x => x.TC_NO == model.TC_NO, false);
-            if (e is not null)
-            {
-                var str = e.TC_NO == model.TC_NO ? "Student Number" : "";
-                return Ok(new ResponseModel()
-                {
-                    IsSuccess = false,
-                    Message = $"There is a student with same {str} number",
-                    Data = null,
-                    StatusCode = 400
-
-                });
-
-            }
-
-            Advisor advisor = new()
-            {
-                Address = model.Address,
-                Email = model.Email,
-                AdvisorName = model.AdvisorName,
-                AdvisorSurname = model.AdviserSurname,
-                TC_NO = model.TC_NO,
-                DepartmentName = model.DepartmentName,
-                ProgramName = model.ProgramName,
-                FacultyName = model.FacultyName,
-            };
-
-
-
-            try
-            {
-                await _advisorWriteRepository.AddAsync(advisor);
-                await _advisorWriteRepository.SaveAsync();
-                return Ok(new ResponseModel()
-                {
-                    IsSuccess = true,
-                    Message = "Successful",
-                    Data = advisor,
-                    StatusCode = 200
-
-                });
-            }
-            catch (Exception ex)
-            {
-                return Ok(new ResponseModel()
-                {
-                    IsSuccess = false,
-                    Message = ex.Message,
-                    Data = null,
-                    StatusCode = 400
-
-                });
-            }
-
-
-            return Ok(new ResponseModel()
-            {
-                IsSuccess = false,
-                Message = "Some problems",
-                Data = null,
-                StatusCode = 499
-
-            });
-
+            CreateAdvisorCommandResponse response= await _mediator.Send(request);
+            return Ok(response);
         }
         [HttpPut]
-        public async Task<IActionResult> Update(VM_Update_Advisor model)
+        public async Task<IActionResult> UpdateAdvisor(UpdateAdvisorCommandRequest request)
         {
-
-            var advisor = await _advisorReadRepository.GetByIdAsync(model.AdvisorID);
-
-            if (advisor is null)
-            {
-                return Ok(new ResponseModel()
-                {
-                    IsSuccess = false,
-                    Message = "Advisor not found",
-                    Data = null,
-                    StatusCode = 400
-
-                });
-            }
-
-            advisor.TC_NO = model.TC_NO;
-            advisor.Email = model.Email;
-            advisor.Address = model.Address;
-            advisor.AdvisorName = model.AdvisorName;
-            advisor.AdvisorSurname = model.AdviserSurname;
-            advisor.DepartmentName = model.DepartmentName;
-            advisor.FacultyName = model.FacultyName;
-            advisor.ProgramName = model.ProgramName;
-            await _advisorWriteRepository.SaveAsync();
-            return Ok(
-               new ResponseModel(true, "Successful", advisor, 200)
-               );
+            UpdateAdvisorCommandResponse response = await _mediator.Send(request);
+            return Ok(response);
+           
         }
         [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(Guid id)
+        public async Task<IActionResult> DeleteAdvisorById(DeleteAdvisorByIdCommandRequest request)
         {
-            var advisor = _advisorReadRepository.GetSingleAsync(x => x.ID == id);
-
-            if (advisor is null)
-            {
-                return Ok(new ResponseModel(false, "Advisor not found", null, 400));
-            }
-            else
-            {
-                advisor.Result.Students.Clear();
-            }
-
-            if (await _advisorWriteRepository.RemoveAsync(id))
-            {
-                await _advisorWriteRepository.SaveAsync();
-
-                return Ok(
-                    new ResponseModel()
-                    {
-                        IsSuccess = true,
-                        Message = "Successfuly advisor deleted",
-                        Data = null,
-                        StatusCode = 200
-                    }
-                    );
-            }
-            else
-            {
-                return Ok(new ResponseModel(false, "Delete is not complete possible id value is not correct", null, 400));
-            }
+            DeleteAdvisorByIdCommandResponse response= await _mediator.Send(request);
+            return Ok(response);
 
         }
 
