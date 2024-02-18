739fed8c8aea48355c1b7b5c7c120b76c804da0c
Taner <Taner@DESKTOP-JD058BL>
2024-02-18 22:42:23 +0300

    update

---
+++ InternshipManagementSystem.API/Core/InternshipManagementSystem.Application/Features/Advisor/Commands/UpdateAdvisor/UpdateAdvisorCommandHandler.cs
@@ -0,0 +1,66 @@
+﻿using InternshipManagementSystem.Application.Repositories;
+using InternshipManagementSystem.Application.ViewModels;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipManagementSystem.Application.Features.Advisor.Commands.UpdateAdvisor
+{
+    public class UpdateAdvisorCommandHandler : IRequestHandler<UpdateAdvisorCommandRequest, UpdateAdvisorCommandResponse>
+    {
+        readonly IAdvisorReadRepository _advisorReadRepository;
+        readonly IAdvisorWriteRepository _advisorWriteRepository;
+
+        public UpdateAdvisorCommandHandler(IAdvisorReadRepository advisorReadRepository, IAdvisorWriteRepository advisorWriteRepository)
+        {
+            _advisorReadRepository = advisorReadRepository;
+            _advisorWriteRepository = advisorWriteRepository;
+        }
+
+        public async Task<UpdateAdvisorCommandResponse> Handle(UpdateAdvisorCommandRequest request, CancellationToken cancellationToken)
+        {
+            var advisor = await _advisorReadRepository.GetByIdAsync(request.AdvisorID);
+
+            if (advisor is null)
+            {
+                return new UpdateAdvisorCommandResponse
+                {
+                    Response= new ResponseModel {
+                        IsSuccess = false,
+                        Message = "Advisor not found",
+                        Data = null,
+                        StatusCode = 400
+                    }
+                    
+
+                };
+            }
+
+            advisor.TC_NO = request.TC_NO;
+            advisor.Email = request.Email;
+            advisor.Address = request.Address;
+            advisor.AdvisorName = request.AdvisorName;
+            advisor.AdvisorSurname = request.AdviserSurname;
+            advisor.DepartmentName = request.DepartmentName;
+            advisor.FacultyName = request.FacultyName;
+            advisor.ProgramName = request.ProgramName;
+
+            await _advisorWriteRepository.SaveAsync();
+            return new UpdateAdvisorCommandResponse
+            {
+                Response = new ResponseModel
+                {
+                    IsSuccess = true,
+                    Message = "Successful",
+                    Data = advisor,
+                    StatusCode = 200
+                }
+
+            };
+              
+        }
+    }
+}
---
+++ InternshipManagementSystem.API/Core/InternshipManagementSystem.Application/Features/Advisor/Queries/GetStudentsOfAllAdvisors/GetStudentsOfAllAdvisorsQueryResponse.cs
@@ -0,0 +1,15 @@
+﻿using InternshipManagementSystem.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipManagementSystem.Application.Features.Advisor.Queries.GetStudentsOfAllAdvisors
+{
+    public class GetStudentsOfAllAdvisorsQueryResponse
+    {
+        public ResponseModel Response { get; set; }
+
+    }
+}
